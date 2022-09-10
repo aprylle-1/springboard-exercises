70b3448f3f0784ba15c4dfe99b47fa158d476d88
aprylle-1 <aprylle.ablaza@gmail.com>
2022-09-11 00:16:10 +0800

    sql ddl design

--- sql-ddl-design/modified_air_traffic.sql
+++ sql-ddl-design/modified_air_traffic.sql
@@ -1,8 +1,8 @@
-DROP DATABASE IF EXISTS air_traffic;
+DROP DATABASE IF EXISTS air_traffic_modified;
 
-CREATE DATABASE air_traffic;
+CREATE DATABASE air_traffic_modified;
 
-\c air_traffic;
+\c air_traffic_modified;
 
 
 -----------------Table for Passenger Names Only-----------------------
@@ -62,33 +62,32 @@ VALUES
 ('Mexico');
 
 ---------------------Table for Availabke Cities in Countries--------------------
-CREATE TABLE countries_cities (
+CREATE TABLE cities (
     id SERIAL PRIMARY KEY,
-    country_id INTEGER REFERENCES countries,
     city TEXT NOT NULL
 );
 
-INSERT INTO countries_cities (country_id, city)
+INSERT INTO cities (city)
 VALUES 
-(1, 'Los Angeles'),
-(1, 'Washington DC'),
-(1, 'Seattle'),
-(1, 'Cedar Rapids'),
-(1, 'New York'),
-(1, 'Charlotte'),
-(2, 'Paris'),
-(3, 'Dubai'),
-(4, 'Tokyo'),
-(5, 'Sao Paolo'),
-(6, 'London'),
-(1, 'Las Vegas'),
-(10, 'Mexico City'),
-(8, 'Casablanca'),
-(7, 'Beijing'),
-(1, 'Charlotte'),
-(1, 'Chicago'),
-(1, 'New Orleans'),
-(9, 'Santiago');
+('Los Angeles'),
+('Washington DC'),
+('Seattle'),
+('Cedar Rapids'),
+('New York'),
+('Charlotte'),
+('Paris'),
+('Dubai'),
+('Tokyo'),
+('Sao Paolo'),
+('London'),
+('Las Vegas'),
+('Mexico City'),
+('Casablanca'),
+('Beijing'),
+('Charlotte'),
+('Chicago'),
+('New Orleans'),
+('Santiago');
 
 ----------------------Table for Tickets------------------------------------
 
@@ -99,22 +98,32 @@ CREATE TABLE tickets (
     departure TIMESTAMP NOT NULL,
     arrival TIMESTAMP NOT NULL,
     airline_id INTEGER REFERENCES airlines,
-    from_location INTEGER REFERENCES countries_cities,
-    to_location INTEGER REFERENCES countries_cities
+    from_city INTEGER REFERENCES cities,
+    from_country INTEGER REFERENCES countries,
+    to_city INTEGER REFERENCES cities,
+    to_country INTEGER REFERENCES countries
 );
 
-INSERT INTO tickets (passenger_id, seat, departure, arrival, airline_id, from_location, to_location)
+INSERT INTO tickets (passenger_id, seat, departure, arrival, airline_id, from_city, from_country, to_city, to_country)
 VALUES 
-    (1, '33B', '2018-04-08 09:00:00', '2018-04-08 12:00:00', 1, 1, 3),
-    (2, '8A', '2018-12-19 12:45:00', '2018-12-19 16:15:00', 5, 9, 11),
-    (3, '12F', '2018-01-02 07:00:00', '2018-01-02 08:03:00', 1, 1, 12),
-    (4, '20A', '2018-04-15 16:50:00', '2018-04-15 21:00:00', 1, 3, 13),
-    (5, '23D', '2018-08-01 18:30:00', '2018-08-01 21:50:00', 3, 7, 14),
-    (6, '18C', '2018-10-31 01:15:00', '2018-10-31 12:55:00', 4, 8, 15),
-    (7, '9E', '2019-02-06 06:00:00', '2019-02-06 07:47:00', 2, 5, 16),
-    (8, '1A', '2018-12-22 14:42:00', '2018-12-22 15:56:00', 7, 4, 17),
-    (9, '32B', '2019-02-06 16:28:00', '2019-02-06 19:18:00', 7, 15, 18),
-    (10, '10D', '2019-01-20 19:30:00', '2019-01-20 22:45:00', 6, 10, 19);
+    (1, '33B', '2018-04-08 09:00:00', '2018-04-08 12:00:00', 1, 2, 1, 3, 1),
+    (2, '8A', '2018-12-19 12:45:00', '2018-12-19 16:15:00', 5, 9, 4, 11, 6),
+    (3, '12F', '2018-01-02 07:00:00', '2018-01-02 08:03:00', 1, 1, 1, 12, 1)
 
 -----------------------------------------------------------------------------
 
+SELECT passengers.first_name, passengers.last_name, 
+seat, departure, arrival, 
+airlines.airline, cities.city as from_city, 
+countries.country as from_country, 
+cities.city as to_city, 
+countries.country as to_country 
+FROM tickets
+JOIN passengers
+ON (tickets.passenger_id = passengers.id)
+JOIN cities
+ON (cities.id = tickets.to_city)
+ON (cities.id = tickets.from_city)
+JOIN countries
+ON (countries.id = tickets.from_country)
+ON (countries.id = tickets.to_country)
